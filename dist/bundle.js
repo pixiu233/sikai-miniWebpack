4858039374f33cc472f6f9db644ecd24d68cbbe3
“526851305@qq.com” <526851305@qq.com>
2023-03-28 20:31:06 +0800

    modules

--- dist/bundle.js
+++ dist/bundle.js
@@ -21,19 +21,38 @@
     
     
     // Load entry module and return exports
-    return __webpack_require__(__webpack_require__.s = "..\src\index.js");
+    return __webpack_require__(__webpack_require__.s = "../src/index.js");
     })
     
     
     ({
     
-        "..\src\index.js":
+        "../src/index.js":
         (function (module, exports,__webpack_require__) {
-    eval(`// const add = require('./add.js')
-import add from './add.js';
+    eval(`const add = __webpack_require__("../src/add.js");
+// import add from './add.js';
 const r = add(1, 2);
 console.log('index');
 module.exports = {};`);
+    }),
+    
+        "../src/add.js":
+        (function (module, exports,__webpack_require__) {
+    eval(`const min = __webpack_require__("../src/min.js");
+const add = (x, y) => {
+  return x + y + min(x, y);
+};
+console.log('add');
+module.exports = add;`);
+    }),
+    
+        "../src/min.js":
+        (function (module, exports,__webpack_require__) {
+    eval(`const min = (x, y) => {
+  return x - y;
+};
+console.log('min');
+module.exports = min;`);
     }),
     
     });
